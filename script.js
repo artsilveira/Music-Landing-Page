231081bf2ae03f0a520accdcb5c5e643caff819c
artsilveira <164525185+artsilveira@users.noreply.github.com>
2026-06-03 17:31:23 -0300

    music section and nav active state

--- script.js
+++ script.js
@@ -19,21 +19,65 @@ document.addEventListener('DOMContentLoaded', function() {
         'navbuytickets': 'Stour'
     };
 
+    function setActiveLink(activeId) {
+
+        Object.keys(navLinks).forEach(function(id) {
+
+            
+            if (id === 'navbuytickets') return;
+
+            document.querySelector('#' + id)
+                .classList.remove(
+                    'font-bold',
+                    'text-white',
+                    'underline',
+                    'underline-offset-4'
+                );
+        });
+
+        if (activeId !== 'navbuytickets') {
+
+            document.querySelector('#' + activeId)
+                .classList.add(
+                    'font-bold',
+                    'text-white',
+                    'underline',
+                    'underline-offset-4'
+                );
+        }
+    }
+
     Object.entries(navLinks).forEach(function(par) {
-        let linkId = par[0];   // 'navmusic'
-        let secId = par[1];    // 'Smusic'
+
+        let linkId = par[0];
+        let secId = par[1];
 
         document.querySelector('#' + linkId).addEventListener('click', function(event) {
+
             event.preventDefault();
+
             showSec(document.querySelector('#' + secId));
+
+            setActiveLink(linkId);
+
         });
     });
 
-    function showSec(secao) {
-        allSec.forEach(function(s) {
-            s.classList.add('hidden');
-        });
-        secao.classList.remove('hidden');
+function showSec(secao) {
+    allSec.forEach(function(s) {
+        s.classList.add('hidden');
+    });
+    secao.classList.remove('hidden');
+
+    const header = document.querySelector('#header');
+    if (secao.id === 'Shome') {
+        header.classList.remove('bg-zinc-950');
+    } else {
+        header.classList.add('bg-zinc-950');
     }
+}  
+
+
+setActiveLink('navhome');
 
 });
